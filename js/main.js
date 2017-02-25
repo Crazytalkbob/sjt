0a8bc6dc3fe8a13364514e10b63fc4fbe38f7dc9
Bobby Speirs <bobby.speirs@gmail.com>
2017-02-24 21:51:46 -0500

    Added scrolling to people cards

--- js/main.js
+++ js/main.js
@@ -1,6 +1,42 @@
-$(document).ready(function(){
-  $('#mobile-menu').click(function(){
-      $('.navbar-content').toggleClass('active');
-      $('#mobile-menu').toggleClass('active');
-  });
+var currentPerson = 0;
+
+$(document).ready(function() {
+    $('#mobile-menu').click(function() {
+        $('.navbar-content').toggleClass('active');
+        $('#mobile-menu').toggleClass('active');
+    });
+
+    $('.navbar-content li a').click(function() {
+        $('.navbar-content').toggleClass('active');
+        $('#mobile-menu').toggleClass('active');
+    });
+
+    $('.people-arrow').click(function() {
+
+        var dir = $(this).data('direction')
+
+        console.log(dir);
+
+        if (currentPerson < 300 && dir === 'right') {
+            currentPerson += 100;
+            if (currentPerson === 300) {
+                $('.right-arrow').removeClass('active');
+            }
+            if (currentPerson === 100) {
+                $('.left-arrow').addClass('active');
+            }
+        } else if (currentPerson > 0 && dir === 'left') {
+            currentPerson -= 100;
+            if (currentPerson === 0) {
+                $('.left-arrow').removeClass('active');
+            }
+            if (currentPerson === 200) {
+                $('.right-arrow').addClass('active');
+            }
+        }
+
+        $('.people-container').css('left', '-' + currentPerson + '%')
+        console.log(currentPerson);
+
+    });
 });
