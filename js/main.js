0c93271ff96051a7fa2b210d80d7d0f0f5918493
Bobby Speirs <bobby.speirs@gmail.com>
2017-02-25 17:22:14 -0500

    Added countdown timer, may need some work

--- js/main.js
+++ js/main.js
@@ -1,6 +1,17 @@
 var currentPerson = 0;
 
+// Variables for countdown timer
+var endDate = 1533735900000,
+    numMilliseconds = [
+        ['days', 1000 * 60 * 60 * 24],
+        ['hours', 1000 * 60 * 60],
+        ['minutes', 1000 * 60],
+        ['seconds', 1000]
+    ];
+
+
 $(document).ready(function() {
+
     $('#mobile-menu').click(function() {
         $('.navbar-content').toggleClass('active');
         $('#mobile-menu').toggleClass('active');
@@ -36,4 +47,33 @@ $(document).ready(function() {
         $('.people-container').css('left', '-' + currentPerson + '%');
 
     });
+
+    function updateTimer() {
+        var time = endDate - Date.parse(new Date()),
+            timerObj = {
+                days: 0,
+                hours: 0,
+                minutes: 0,
+                seconds: 0
+            };
+        if (time < 0) {
+            clearInterval(updateTimer);
+            return;
+        }
+        numMilliseconds.forEach(function(val) {
+            while (time >= val[1]) {
+                timerObj[val[0]] += 1;
+                time -= val[1];
+            }
+        });
+
+        $('#days').html(timerObj.days);
+        $('#hours').html(timerObj.hours);
+        $('#minutes').html(timerObj.minutes);
+        $('#seconds').html(timerObj.seconds);
+    }
+
+    updateTimer();
+    var countdownInterval = setInterval(updateTimer, 1000);
+
 });
